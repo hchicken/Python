45f36ac6dd2fdf3589b7e19861b0570b1e56e20e
tanzhihao <tanzhihao@cyou-inc.com>
2019-12-31 20:58:30 +0800

    实现strStr

---
+++ code/2019_12/实现strStr.py
@@ -0,0 +1,40 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author : Chicken dishes
+# Created Time : 2019/12/31 20:37
+
+
+"""
+给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+"""
+
+
+# class Solution:
+#     def strStr(self, haystack: str, needle: str) -> int:
+#         if needle in haystack:
+#             res = haystack.index(needle)
+#             return res
+#         else:
+#             return -1
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+        len_needle = len(needle)
+        len_haystack = len(haystack)
+        index = 0
+        while len_haystack - len_needle >= index:
+            if haystack[index:index + len_needle] == needle:
+                print(index)
+                return index
+            else:
+                index += 1
+        return -1
+
+
+if __name__ == "__main__":
+    my_class = Solution()
+    haystack = "mississippi"
+    needle = "issip"
+    my_def = my_class.strStr(haystack=haystack, needle=needle)
