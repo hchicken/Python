3d0c247141b7b8bd8890e06f8a17750948da1a1b
tanzhihao <l>
2020-01-07 22:19:10 +0800

    没有直接算快

--- code/2020_01/最后一个单词的长度.py
+++ code/2020_01/最后一个单词的长度.py
@@ -10,15 +10,24 @@
 
 
 class Solution:
+    # def lengthOfLastWord(self, s: str) -> int:
+    #     s = s.strip()
+    #     last_s = s.split(" ")[-1]
+    #     last_len = len(last_s)
+    #     return last_len
+
     def lengthOfLastWord(self, s: str) -> int:
         s = s.strip()
-        last_s = s.split(" ")[-1]
-        last_len = len(last_s)
-        return last_len
-
+        i = 0
+        for str_my in s[::-1]:
+            if str_my != " ":
+                i += 1
+            else:
+                break
+        return i
 
 if __name__ == "__main__":
     my_class = Solution()
-    s = "a         "
+    s = "a dasdas"
     my_def = my_class.lengthOfLastWord(s)
     print(my_def)
