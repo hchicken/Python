97c7f52fa04c36aeade161967216f053786d715d
tanzhihao <l>
2020-05-26 21:06:18 +0800

    瞎写的

---
+++ code/2020_05/删除排序链表中的重复元素.py
@@ -0,0 +1,41 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author : Chicken dishes
+# Created Time : 2020/5/26 20:26
+
+
+"""
+给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+
+示例 1:
+
+输入: 1->1->2
+输出: 1->2
+示例 2:
+
+输入: 1->1->2->3->3
+输出: 1->2->3
+
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        node = head
+        while node and node.next:
+            if node.val == node.next.val:
+                node.next = node.next.next
+            else:
+                node = node.next
+        return head
+
+
+if __name__ == "__main__":
+    pass
