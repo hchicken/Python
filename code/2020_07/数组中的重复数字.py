771637bdb7c9905312117554ae264c47320ca877
dd-tan <342321394@qq.com>
2020-07-02 22:15:17 +0800

    重复数字打印

---
+++ code/2020_07/数组中的重复数字.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/7/2 21:53
+# @Author  : Greens
+
+
+"""
+在一个长度为n的数组里的所有数字都在0到n-1的范围内。 数组中某些数字是重复的，
+但不知道有几个数字是重复的。也不知道每个数字重复几次。请找出数组中任意一个重复的数字。
+例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是第一个重复的数字2。
+"""
+
+
+class Solution:
+    # 这里要特别注意~找到任意重复的一个值并赋值到duplication[0]
+    # 函数返回True/False
+    def duplicate(self, numbers, duplication):
+        my_list = []
+        for i in range(len(numbers)):
+            aaa = numbers[i]
+            if aaa in my_list:
+                duplication[0] = aaa
+                return True
+            my_list.append(aaa)
+        return False
+
+
+if __name__ == '__main__':
+    my_class = Solution()
+    my_list = [2, 3, 1, 0, 2, 5, 3]
+    my_class.duplicate(my_list, [])
